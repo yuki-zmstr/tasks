d71e3604de56a64e4220c2a86e50aade790f390f
okkhoy <okkhoy@gmail.com>
2024-02-23 10:24:28 +0800

    Add datamanager class

---
+++ src/main/java/se/edu/streamdemo/data/DataManager.java
@@ -0,0 +1,104 @@
+package se.edu.streamdemo.data;
+
+import se.edu.streamdemo.task.Deadline;
+import se.edu.streamdemo.task.Event;
+import se.edu.streamdemo.task.Task;
+import se.edu.streamdemo.task.Todo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataManager {
+    private File dataFile;
+
+    public File getDataFile() {
+        return dataFile;
+    }
+
+    public DataManager(String fileName) {
+        dataFile = new File(fileName);
+    }
+
+    public void createFile() {
+        try {
+            if (dataFile.exists()) {
+                System.out.println("file exists");
+                return;
+            }
+            if (!dataFile.getParentFile().exists()) {
+                dataFile.getParentFile().mkdirs();
+            }
+            dataFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Cannot create file; reason: " + e.getMessage());
+        }
+    }
+
+    private ArrayList readFile() throws IOException {
+        if (!dataFile.exists()) {
+            throw new FileNotFoundException();
+        }
+        if (dataFile.length() == 0) {
+            System.out.println("empty file");
+            throw new IOException();
+        }
+        ArrayList<String> dataItems = (ArrayList) Files.readAllLines(dataFile.toPath(), Charset.defaultCharset());
+
+        return dataItems;
+    }
+
+    public ArrayList<Task> loadData() {
+        ArrayList<Task> taskList = null;
+        try {
+            ArrayList<String> dataItems = readFile();
+            taskList = parse(dataItems);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return taskList;
+    }
+
+    private ArrayList<Task> parse(ArrayList<String> dataItems) {
+        ArrayList<Task> allTasks = new ArrayList<>();
+        for (String line : dataItems) {
+            String taskDescription = getTaskDescription(line);
+            String taskType = getTaskType(line);
+            switch (taskType) {
+                case "T":
+                    Todo todo = new Todo(taskDescription);
+                    allTasks.add(todo);
+                    break;
+                case "D":
+                    Deadline deadline = new Deadline(taskDescription);
+                    allTasks.add(deadline);
+                    break;
+                case "E":
+                    Event event = new Event(taskDescription);
+                    allTasks.add(event);
+                    break;
+                default:
+                    System.out.println("Unknown task encountered. Skipping");
+                    break;
+            }
+        }
+        return allTasks;
+    }
+
+    private static String getTaskType(String inputLine) {
+        String taskType = inputLine.substring(0, 2);
+        taskType = taskType.replace("[", "");
+        taskType = taskType.replace("]", "");
+        return taskType;
+    }
+
+    private static String getTaskDescription(String inputLine) {
+        String taskDescription = inputLine.substring(4);
+        return taskDescription;
+    }
+}
