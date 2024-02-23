e72bfa90043c3955e6bd8d35396c827939673603
okkhoy <okkhoy@gmail.com>
2024-02-23 10:28:47 +0800

    Add functional code to Main

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -1,7 +1,21 @@
 package se.edu.streamdemo;
 
+import se.edu.streamdemo.data.DataManager;
+import se.edu.streamdemo.task.Task;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Task (stream) manager");
+        DataManager dataManager = new DataManager("./data/data.txt");
+        ArrayList<Task> tasksData = dataManager.loadData();
+        printAllData(tasksData);
+    }
+
+    public static void printAllData(ArrayList<Task> tasksData) {
+        for (Task t : tasksData) {
+            System.out.println(t);
+        }
     }
 }
