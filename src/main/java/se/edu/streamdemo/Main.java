674152caa593f36e3fdd6294c3d75f0dfc5957a8
okkhoy <okkhoy@gmail.com>
2024-02-23 10:32:17 +0800

    Add code to count deadlines and print deadlines

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -1,6 +1,7 @@
 package se.edu.streamdemo;
 
 import se.edu.streamdemo.data.DataManager;
+import se.edu.streamdemo.task.Deadline;
 import se.edu.streamdemo.task.Task;
 
 import java.util.ArrayList;
@@ -10,7 +11,25 @@ public class Main {
         System.out.println("Welcome to Task (stream) manager");
         DataManager dataManager = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dataManager.loadData();
+
+        System.out.println("Printing all data ...");
         printAllData(tasksData);
+
+        System.out.println("Printing deadlines ...");
+        printDeadlines(tasksData);
+
+        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+
+    }
+
+    private static int countDeadlines(ArrayList<Task> tasksData) {
+        int count = 0;
+        for (Task t : tasksData) {
+            if (t instanceof Deadline) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void printAllData(ArrayList<Task> tasksData) {
@@ -18,4 +37,13 @@ public class Main {
             System.out.println(t);
         }
     }
+
+    public static void printDeadlines(ArrayList<Task> tasksData) {
+        for (Task t : tasksData) {
+            if (t instanceof Deadline) {
+                System.out.println(t);
+            }
+        }
+    }
+
 }
